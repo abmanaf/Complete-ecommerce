7978bf60c4fcc6bf12a23caa858e276580bbdc33
Alibaba <abdulmanafaliu414@gmail.com>
2024-03-13 05:58:52 +0000

    changes

---
+++ src/Component/Cart_Icon/CartIcon.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const CartIcon = ({ cartCount }) => {
+  return (
+    <div>
+      <Link
+        to="/E-commerce-react/CartContent"
+        className="cart-icon"
+        style={{ fontSize: "20px" }}
+      >
+        <i className="fa fa-shopping-cart"></i>
+        <span
+          className="cart-badge"
+          style={{
+            textAlign: "center",
+            alignItems: "center",
+            justifyContent: "center",
+            display: "flex",
+          }}
+        >
+          {cartCount}
+        </span>
+      </Link>
+    </div>
+  );
+};
+
+export default CartIcon;
