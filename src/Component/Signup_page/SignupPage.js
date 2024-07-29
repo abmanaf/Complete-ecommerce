17b00bad7382052ced106e4fa539068568732994
Alibaba <abdulmanafaliu414@gmail.com>
2024-07-29 16:09:35 +0000

    I removed unneccessary codes

--- src/Component/Signup_page/SignupPage.js
+++ src/Component/Signup_page/SignupPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
 import "./SignupPage.css";
 import Toast from "../Toaste/Toast";
 import toast, { Toaster } from "react-hot-toast";
