b1775916602b794004fbc9b8f413b906cb197250
Alibaba <abdulmanafaliu414@gmail.com>
2024-07-29 15:36:01 +0000

    I created a toast component

---
+++ src/Component/Toaste/Toast.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Toast.css";
+
+const Toast = () => {
+  return (
+    <div className="toast-container">
+      <h2 className="toast-title">
+        <img src="/assets/images/icon-success.svg" />
+        <span style={{ color: "white" }}>Account Created</span>
+      </h2>
+      <p style={{ fontSize: "10px" }} className="toast-desc">
+        Thanks for Creating an account with us. Welcome to the soceity!
+      </p>
+    </div>
+  );
+};
+
+export default Toast;
