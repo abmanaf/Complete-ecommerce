8bf52c5ff6260799a9bf54a38c65564bf80b3dbe
Alibaba <abdulmanafaliu414@gmail.com>
2024-07-29 15:34:10 +0000

    I romoved commented codes

--- src/Component/Signup_page/SignupPage.js
+++ src/Component/Signup_page/SignupPage.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./SignupPage.css";
+import Toast from "../Toaste/Toast";
+import toast, { Toaster } from "react-hot-toast";
 
 function SignupPage({ updateEnteredDetails }) {
   const [firstName, setFirstName] = useState("");
@@ -33,6 +35,7 @@ function SignupPage({ updateEnteredDetails }) {
 
     const isValid = !Object.values(newError).some((value) => value);
     if (isValid) {
+      toast((t) => <Toast onClick={() => toast.dismiss(t.id)} />);
       const newUser = {
         firstName: firstName,
         lastName: lastName,
@@ -46,96 +49,112 @@ function SignupPage({ updateEnteredDetails }) {
       setLastName("");
       setEmail("");
       setPassword("");
-      navigate("/Data");
+      //navigate("/Data");
+      //alert("Information added");
     }
   };
 
   return (
-    <div className="signup-pic-and-forms">
-      <div className="signup-emoji"></div>
-      <div className="form-container" style={{ marginTop: "8em" }}>
-        <h2 style={{ textAlign: "center" }}>Create Account</h2>
-        <form onSubmit={handleSubmitForms}>
-          <div className="input-container">
-            <input
-              type="text"
-              name="first_name"
-              className={`form-input ${error.firstName ? "error" : ""}`}
-              placeholder="First name"
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-            />
-            {/*
+    <>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: "var(  --Green-medium)",
+            color: "#fff",
+          },
+        }}
+      />
+      <div className="signup-pic-and-forms">
+        {/*
+        <div className="signup-emoji"></div>
+        */}
+        <div className="form-container" style={{ marginTop: "8em" }}>
+          <h2 style={{ textAlign: "center" }}>Create Account</h2>
+          <form onSubmit={handleSubmitForms}>
+            <div className="input-container">
+              <input
+                type="text"
+                name="first_name"
+                className={`form-input ${error.firstName ? "error" : ""}`}
+                placeholder="First name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+              {/*
             {error.firstName && (
               <span className="error-message">This field is required</span>
             )}
                */}
-          </div>
-          <div className="input-container">
-            <input
-              type="text"
-              name="last_name"
-              className={`form-input ${error.lastName ? "error" : ""}`}
-              placeholder="Last name"
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-            />
-            {/* 
+            </div>
+            <div className="input-container">
+              <input
+                type="text"
+                name="last_name"
+                className={`form-input ${error.lastName ? "error" : ""}`}
+                placeholder="Last name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+              {/* 
             {error.lastName && (
               <span className="error-message">This field is required</span>
             )}
               */}
-          </div>
-          <div className="input-container">
-            <input
-              type="email"
-              name="email"
-              className={`form-input ${error.email ? "error" : ""}`}
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            {/* 
+            </div>
+            <div className="input-container">
+              <input
+                type="email"
+                name="email"
+                className={`form-input ${error.email ? "error" : ""}`}
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              {/* 
             {error.email && (
               <span className="error-message">Invalid email address</span>
             )}
               */}
-          </div>
-          <div className="input-container">
-            <input
-              type="password"
-              name="password"
-              className={`form-input ${error.password ? "error" : ""}`}
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            {/* 
+            </div>
+            <div className="input-container">
+              <input
+                type="password"
+                name="password"
+                className={`form-input ${error.password ? "error" : ""}`}
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {/* 
             {error.password && (
               <span className="error-message">Check the password criteria</span>
             )}
               */}
-          </div>
-          <p style={{ fontSize: "14px", color: "#777777" }}>
-            Your password must:
-          </p>
-          <ul style={{ fontSize: "14px", color: "#777777" }}>
-            <li>Be at least 6 characters long</li>
-            <li>Not be the same as your name or email</li>
-          </ul>
-          <br />
-          <button type="submit" className="form-button">
-            Create new account
-          </button>
-          <p className="form-footer" style={{ textAlign: "center" }}>
-            Already have an account?{" "}
-            <Link to="/LoginPage" style={{ color: "#007bff" }}>
-              Login
-            </Link>
-          </p>
-        </form>
+            </div>
+            <p style={{ fontSize: "14px", color: "#777777" }}>
+              Your password must:
+            </p>
+            <ul style={{ fontSize: "14px", color: "#777777" }}>
+              <li>Be at least 6 characters long</li>
+              <li>Not be the same as your name or email</li>
+            </ul>
+            <br />
+            <button type="submit" className="form-button">
+              Create new account
+            </button>
+            <p className="form-footer" style={{ textAlign: "center" }}>
+              Already have an account?{" "}
+              <Link to="/LoginPage" style={{ color: "#007bff" }}>
+                Login
+              </Link>
+            </p>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
