329375168445f0ade28e5711ed95010e5055db5d
Alibaba <abdulmanafaliu414@gmail.com>
2024-09-03 13:38:24 +0000

    Modal component created

---
+++ src/Component/Modal/Modal.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Modal.css";
+
+function Modal({ show, onClose, confirmDelete, selectedProduct }) {
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <div style={{ textAlign: "start" }}>
+          <p style={{ textAlign: "center" }}>
+            {`Are you sure you want to remove ${selectedProduct.name} from the cart? `}
+          </p>
+        </div>
+        <div
+          className="modal-button"
+          style={{
+            marginTop: "2em",
+            display: "flex",
+            justifyContent: "center",
+            gap: "1em",
+          }}
+        >
+          <button
+            style={{ backgroundColor: "green" }}
+            onClick={onClose}
+            className="modal-button"
+          >
+            Cancel
+          </button>
+          <button
+            style={{ backgroundColor: "red" }}
+            onClick={confirmDelete}
+            className="modal-button"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
