1bd06678000fcb0738b502a5701e0c8c0098a50b
Alibaba <abdulmanafaliu414@gmail.com>
2024-09-03 13:38:39 +0000

    changes made

--- src/Component/Cart_Content/CartContent.js
+++ src/Component/Cart_Content/CartContent.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Imageurl from "../Photoss/Photo";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./CartContent.css";
+import Modal from "../Modal/Modal";
 
 const CartContent = ({ cart, updateCart, updateCartCount }) => {
   const navigate = useNavigate();
-  //const [selectedProduct, setSelectedProduct] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
   const increaseQuantity = (productId) => {
     const updatedCart = cart.map((product) => {
@@ -43,15 +45,21 @@ const CartContent = ({ cart, updateCart, updateCartCount }) => {
 
   const removeFromCart = (productId) => {
     const productToRemove = cart.find((product) => product.id === productId);
+    if (productToRemove) {
+      setSelectedProduct(productToRemove);
+      setShowModal(true);
+    }
+  };
 
-    const confirmDelete = window.confirm(
-      `Are you sure you want to remove ${productToRemove.name} from the cart?`
-    );
-
-    if (confirmDelete) {
-      const updatedCart = cart.filter((product) => product.id !== productId);
+  const confirmDelete = () => {
+    if (selectedProduct) {
+      const updatedCart = cart.filter(
+        (product) => product.id !== selectedProduct.id
+      );
       updateCart(updatedCart);
       updateCartCount(calculateTotalCount(updatedCart));
+      setShowModal(false);
+      setSelectedProduct(null);
     }
   };
 
@@ -81,7 +89,6 @@ const CartContent = ({ cart, updateCart, updateCartCount }) => {
           <img
             src="https://cdni.iconscout.com/illustration/premium/thumb/empty-cart-2130356-1800917.png"
             alt="cart"
-           
           />{" "}
           <br />
           <strong className="empty-cart-text">Cart is empty</strong>
@@ -101,76 +108,76 @@ const CartContent = ({ cart, updateCart, updateCartCount }) => {
                   <div
                     style={{
                       display: "flex",
-                      width: "75%",
-                      padding: "1em 1em",
+                      justifyContent: "space-around",
+                      alignItems: "center",
                     }}
                   >
-                    <div></div>
-                    <div>
-                      <span className="table-sell-style">
-                        <img src={Imageurl(product)} alt={product.id} />
-                      </span>
-                    </div>
-                    <div>
-                      <span className="table-sell-style">{product.name}</span>{" "}
-                      <span
-                        className="table-sell-style"
-                        style={{ float: "right" }}
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                      }}
+                    >
+                      <div>
+                        <span className="table-sell-style">
+                          <img src={Imageurl(product)} alt={product.id} />
+                        </span>
+                      </div>
+                      <div
+                        style={{
+                          flexDirection: "column",
+                          justifyContent: "space-around",
+                          alignItems: "center",
+                        }}
                       >
+                        <div>
+                          <span
+                            style={{ marginBottom: "5px" }}
+                            className="table-sell-style"
+                          >
+                            {product.name}
+                          </span>
+                        </div>
+                        <div>
+                          <span style={{ marginBottom: "5px" }}>Price</span>
+                        </div>
+                        <div>
+                          <span style={{ marginBottom: "5px" }}>SubTotal</span>
+                        </div>
+                        <div>
+                          <span style={{ marginBottom: "5px" }}>Quantity</span>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      style={{ flexDirection: "column", textAlign: "center" }}
+                    >
+                      <div className="price-style">
                         <button
                           onClick={() => removeFromCart(product.id)}
                           className="action-button-style"
                         >
                           <i className="fa fa-times" aria-hidden="true"></i>
                         </button>
-                      </span>
-                      <br />
-                      <br />
-                      <div
-                        style={{
-                          display: "flex",
-                          justifyContent: "space-between",
-                        }}
-                      >
-                        <span>Price</span>
+                      </div>
+
+                      <div>
                         <span className="price-style">{product.price}</span>
                       </div>
-                      <br />
-                      <div
-                        style={{
-                          display: "flex",
-                          justifyContent: "space-between",
-                        }}
-                      >
-                        <span>SubTotal</span>
+                      <div>
                         <span className="price-style">
                           {(product.count * parseFloat(product.price)).toFixed(
                             2
                           )}
                         </span>
-                        <br />
-                        <br />
                       </div>
-                      <div
-                        style={{
-                          display: "flex",
-                          justifyContent: "space-between",
-                        }}
-                      >
-                        <div style={{ marginTop: "1px" }}>
-                          <span>Quantity</span>
-                        </div>
-                        <div
-                          style={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                          }}
-                        >
+                      <div>
+                        <div></div>
+                        <div className="price-style">
                           <span
                             style={{
                               textAlign: "center",
-                              marginLeft: "6em",
                             }}
                           >
                             <div className="cart-border">
@@ -178,7 +185,6 @@ const CartContent = ({ cart, updateCart, updateCartCount }) => {
                                 onClick={() => reduceQuantity(product.id)}
                                 className="quantity-button-decrease"
                                 style={{
-                                  marginRight: "10px",
                                   textDecoration: "none",
                                 }}
                               >
@@ -189,7 +195,6 @@ const CartContent = ({ cart, updateCart, updateCartCount }) => {
                                 onClick={() => increaseQuantity(product.id)}
                                 className="quantity-button-increase"
                                 style={{
-                                  marginLeft: "10px",
                                   textDecoration: "none",
                                 }}
                               >
@@ -248,6 +253,15 @@ const CartContent = ({ cart, updateCart, updateCartCount }) => {
           </div>
         </div>
       )}
+      <Modal
+        show={showModal}
+        onClose={() => {
+          setShowModal(false);
+          setSelectedProduct(null);
+        }}
+        confirmDelete={confirmDelete}
+        selectedProduct={selectedProduct}
+      />
     </div>
   );
 };
