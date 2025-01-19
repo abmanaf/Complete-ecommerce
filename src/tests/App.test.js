bf038d36d6187ce64c20895cc78be91806a823b3
abmanaf <aliu.manaf@amalitech.com>
2025-01-19 09:19:54 +0000

    refactor: wrapped the App component with BrowserRouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from '../App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText(/Welcome to AlibabShop/i);
   expect(linkElement).toBeInTheDocument();
 });
