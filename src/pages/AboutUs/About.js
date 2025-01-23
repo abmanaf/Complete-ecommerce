0975fafce231c61b7bf587adabd56c2b1b023c30
abmanaf <aliu.manaf@amalitech.com>
2025-01-23 19:24:05 +0000

    refactor: changed the entire design

--- src/pages/AboutUs/About.js
+++ src/pages/AboutUs/About.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
-import Image from "../../Components/atoms/Image";
-import about_us from "./about__us.avif";
 import "./About.css";
-import Button from "../../Components/atoms/button/Button";
+import aboutUs from "./about__us.avif";
 
 function About() {
   const [showMore, setShowMore] = useState(false);
@@ -13,49 +11,63 @@ function About() {
 
   return (
     <div className="about-container">
-      <div className="about-content">
-        <h2 className="about-heading">About Us</h2>
-        <p className="about-text">
-          {showMore ? (
-            <article>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
+      <div className="about-wrapper">
+        <div className="about-content">
+          <h2 className="about-heading">About Our Company</h2>
+          
+          <div className="about-text">
+            {showMore ? (
+              <>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting
+                  industry. Lorem Ipsum has been the industry's standard dummy text
+                  ever since the 1500s, when an unknown printer took a galley of
+                  type and scrambled it to make a type specimen book. It has
+                  survived not only five centuries, but also the leap into
+                  electronic typesetting, remaining essentially unchanged.
+                </p>
+                <p>
+                  It was popularised in the 1960s with the release of Letraset 
+                  sheets containing Lorem Ipsum passages, and more recently with 
+                  desktop publishing software like Aldus PageMaker including 
+                  versions of Lorem Ipsum.
+                </p>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting
+                  industry. Lorem Ipsum has been the industry's standard dummy
+                  text ever since the 1500s, when an unknown printer took a galley
+                  of type and scrambled it to make a type specimen book.
+                </p>
+              </>
+            ) : (
               <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
+                industry. Lorem Ipsum has been the industry's standard dummy text
+                ever since the 1500s when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book. It has survived not
+                only five centuries, but also the leap into electronic
+                typesetting...
               </p>
-            </article>
-          ) : (
-            <article>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s when an unknown printer took a galley of type
-              and scrambled it to make a type specimen book. It has survived not
-              only five centuries, but also the leap into electronic
-              typesetting...
-            </article>
-          )}
-        </p>
-        <Button
-          className="submit-message"
-          type="submit"
-          onClick={handleShowMore}
-        >
-          {showMore ? "Show Less" : "Show more"}
-        </Button>
-      </div>
-      <div className="about-image">
-        <Image src={about_us} alt="about us" />
+            )}
+          </div>
+          
+          <button 
+            className="show-more-btn" 
+            onClick={handleShowMore}
+          >
+            {showMore ? "Show Less" : "Show More"}
+          </button>
+        </div>
+        
+        <div className="about-image-container">
+          <div className="about-image-wrapper">
+            <img 
+              src={aboutUs} 
+              alt="About Us" 
+              className="about-image" 
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
