c28d6fef304a90f29cbbf8b38e031c01341de0f9
abmanaf <aliu.manaf@amalitech.com>
2025-02-18 16:04:07 +0000

    refactor: imported useSelector and useDispatch to work on the data manipulation

--- src/Components/organism/ShoppingPage/Shop.js
+++ src/Components/organism/ShoppingPage/Shop.js
@@ -1,84 +1,46 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Imageurl from "../../atoms/Photoss/Photo";
-import { initialProducts } from "../../../Data/Database";
 import "./Shop.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Button from "../../atoms/button/Button";
+import { useSelector, useDispatch } from "react-redux";
+import { addToCart } from "../../../state/slices/cartSlice";
 
-const Shop = ({ cart, setCart, updateCartCount }) => {
-  const [products, setProducts] = useState(initialProducts);
+const Shop = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products.products);
+  const cartItems = useSelector((state) => state.cart.items);
   const [selectedCategory, setSelectedCategory] = useState("all");
-  const [selectedProductIds, setSelectedProductIds] = useState([]);
 
-  const filteredProducts = initialProducts.filter((product) => {
+  const filteredProducts = products.filter((product) => {
     return selectedCategory === "all" || product.category === selectedCategory;
   });
 
-  const addToCart = (productId) => {
+  const handleAddToCart = (productId) => {
     const product = products.find((product) => product.id === productId);
-    if (product) {
-      if (product.availableProduct > 0) {
-        const productInCart = cart.find((item) => item.id === productId);
-        if (!productInCart) {
-          setSelectedProductIds((prevSelected) => {
-            if (!prevSelected.includes(productId)) {
-              return [...prevSelected, productId];
-            }
-            return prevSelected;
-          });
-
-          toast.success(`${product.name} added to cart!`);
-
-          const updatedProducts = products.map((product) => {
-            if (product.id === productId) {
-              return {
-                ...product,
-                count: Number(product.count) + 1,
-              };
-            }
-            return product;
-          });
-
-          setProducts(updatedProducts);
-          updateCartCount(calculateTotalCount(updatedProducts));
-
-          const updatedCart = updatedProducts.filter(
-            (product) => product.count > 0
-          );
-          setCart(updatedCart);
-        } else {
-          toast.error(`${product.name} is already in the cart!`);
-        }
-      } else {
-        toast.error(`${product.name} is out of stock!`);
-      }
-    }
-  };
-
-  const calculateTotalCount = (cart) => {
-    return cart.reduce((count, product) => count + product.count, 0);
+    const cartItem = cartItems.find((item) => item.id === productId);
+    if (cartItem && cartItem.count >= product.availableProduct) {
+      toast.error(`${product.name} is out of stock!`);
+      return
+    } 
+    dispatch(addToCart(product));
+    toast.success(`${product.name} added to cart`);
+    
   };
 
   const productList = filteredProducts.map((product) => (
     <div key={product.id} className="product-container">
-      <li
-        className={`list ${
-          selectedProductIds.includes(product.id) ? "border" : ""
-        }`}
-      >
+      <li>
         <div className="sub-product-container">
           <img src={Imageurl(product)} alt={product.id} />
           <br />
           {product.name} <br />
-          <del>
-            {product.previousPrice ? `₵${product.previousPrice}` : ""}
-          </del>{" "}
-          &cent;{product.price}
+          <del>{product.previousPrice ? `₵${product.previousPrice}` : ""}</del> &cent;{product.price}
           <br />
           <br />
           <div className="add_to_cart">
-            <Button onClick={() => addToCart(product.id)}>Add To Cart</Button>
+            <Button onClick={() => handleAddToCart(product.id)}>Add To Cart</Button>
           </div>
         </div>
       </li>
@@ -92,27 +54,21 @@ const Shop = ({ cart, setCart, updateCartCount }) => {
         <div className="category-container">
           <div className="filter-button" code="categoryFilter">
             <Button
-              className={`feature-all ${
-                selectedCategory === "all" ? "active" : ""
-              }`}
+              className={`feature-all ${selectedCategory === "all" ? "active" : ""}`}
               onClick={() => setSelectedCategory("all")}
             >
               <i className="fa fa-globe" aria-hidden="true"></i>
               All
             </Button>
             <Button
-              className={`feature-fruits ${
-                selectedCategory === "fruits" ? "active" : ""
-              }`}
+              className={`feature-fruits ${selectedCategory === "fruits" ? "active" : ""}`}
               onClick={() => setSelectedCategory("fruits")}
             >
               <i className="fa fa-weibo" aria-hidden="true"></i>
               Fruits
             </Button>
             <Button
-              className={`feature-vegetables ${
-                selectedCategory === "vegetables" ? "active" : ""
-              }`}
+              className={`feature-vegetables ${selectedCategory === "vegetables" ? "active" : ""}`}
               onClick={() => setSelectedCategory("vegetables")}
             >
               <i className="fa fa-cutlery" aria-hidden="true"></i>
